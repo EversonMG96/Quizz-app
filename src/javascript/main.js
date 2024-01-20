60b5ea53d162d62c04aa64a529aa3884638b704c
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-20 16:38:32 -0300

    add new question in the main,js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,6 +3,20 @@ const questions = [
         question: "Whitc is larget animal in the world?",
         answers: [
             {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false}
+        ]
+    },
+    
+    {
+        question: "Whitc is larget animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false}
         ]
     }
+
 ];
