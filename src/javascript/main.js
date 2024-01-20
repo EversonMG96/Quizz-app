7dd2696100efbf1b253534978ae1056fde279c4d
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-20 18:21:56 -0300

    create function selectAnswer and add parameters

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -85,8 +85,13 @@ function resetState() {
     } 
 }
 
-function selectAnswer() {
-    
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
+    if(isCorrectsCorrect) {
+
+    }
 }
 
 starQuiz();
