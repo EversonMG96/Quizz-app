65e38fd0a160eafa8834744591f411bf283e6771
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-20 18:17:47 -0300

    add structur of control  and create function selectAnswer

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -66,14 +66,27 @@ function showQuestion() {
        button.innerHTML = answer.text; 
        button.classList.add("btn");
        answerButtons.appendChild(button);
+       
+       if (answer.correct) {
+        button.dataset.correct = answer.correct;
+
+        }
+       
+        button.addEventListener("click", selectAnswer);
+
     });
 }
 
 function resetState() {
     nextButton.style.display = "none";
+    
     while(answerButtons.firstChild){
        answerButtons.removeChild(answerButtons.firstChild); 
     } 
+}
+
+function selectAnswer() {
+    
 }
 
 starQuiz();
