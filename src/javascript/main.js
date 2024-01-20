3ab87d708ce5fd6eebab31f82a5242a2fee9e1af
Everson <138945004+EversonMG96@users.noreply.github.com>
2024-01-20 16:40:54 -0300

    add +2 questions in the main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -17,6 +17,28 @@ const questions = [
             {text: "Elephant", correct: false},
             {text: "Giraffe", correct: false}
         ]
+    },
+
+    {
+        question: "Whitc is larget animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false}
+        ]
+    },
+
+    {
+        question: "Whitc is larget animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false}
+        ]
     }
 
 ];
+
+const questionElement = document.getElementById("");
